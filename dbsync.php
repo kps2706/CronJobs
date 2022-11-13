8104bb62932c7e9b730027581472f20a3f9ef792
kps2706 <kps2706@gmail.com>
2022-11-13 06:21:11 +0530

    connection

--- dbsync.php
+++ dbsync.php
@@ -5,6 +5,9 @@ try {
     $pdo_hp = new PDO('mysql:host=localhost;dbname=cashProDB', 'root', '');
 
     // echo "connection successful";
+
+    echo $pdo_mac->getAttribute(PDO::ATTR_CONNECTION_STATUS);
+
     $pdo_mac->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 } catch (PDOException $e) {
     echo $e->getMessage();
