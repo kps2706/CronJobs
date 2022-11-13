550b840772adba55bae73a49aecbd80c781c987a
kps2706 <kps2706@gmail.com>
2022-11-13 06:33:34 +0530

    Rowcount of tables

--- dbsync.php
+++ dbsync.php
@@ -6,9 +6,30 @@ try {
 
     // echo "connection successful";
 
-    echo $pdo_mac->getAttribute(PDO::ATTR_CONNECTION_STATUS);
+    // echo $pdo_mac->getAttribute(PDO::ATTR_CONNECTION_STATUS);
 
     $pdo_mac->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+
+    $sql_mac = "Select * from tbl_cashflow";
+
+    $result_mac = $pdo_mac->query($sql_mac);
+
+    $sql_hp = "Select * from tbl_cashflow";
+
+    $result_hp = $pdo_hp->query($sql_hp);
+
+    if ($result_mac->rowCount() == $result_hp->rowCount()) {
+        // echo "DB Sync";
+    } else {
+        // echo "DB not Sync";
+
+        if ($result_mac->rowCount() > $result_hp->rowCount()) {
+            // echo "Db Sync is started from MacBook to HP";
+        } else {
+            // echo "Db Sync is started from HP to Macbook";
+        }
+    }
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
